2497dbb78312d4231927f40082e08d55e02175bc
bonomani <bonomani@users.noreply.github.com>
2021-06-30 17:05:49 +0200

    [PHP 7.2] Deprecated: assert(): Calling assert() with a string argument is deprecated

--- lib/Auth/Source/SQL.php
+++ lib/Auth/Source/SQL.php
@@ -59,8 +59,8 @@ class sspmod_sqlauthBcrypt_Auth_Source_SQL extends sspmod_core_Auth_UserPassBase
 	 * @param array $config	 Configuration.
 	 */
 	public function __construct($info, $config) {
-		assert('is_array($info)');
-		assert('is_array($config)');
+		assert(is_array($info));
+		assert(is_array($config));
 
 		/* Call the parent constructor first, as required by the interface. */
 		parent::__construct($info, $config);
@@ -138,8 +138,8 @@ class sspmod_sqlauthBcrypt_Auth_Source_SQL extends sspmod_core_Auth_UserPassBase
 	 * @return array	Associative array with the users attributes.
 	 */
 	protected function login($username, $password) {
-		assert('is_string($username)');
-		assert('is_string($password)');
+		assert(is_string($username));
+		assert(is_string($password));
 
 		$db = $this->connect();
 
